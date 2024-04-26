7df2c9410a30e0abf5da01f1c1d4aac388f2fad8
albecannoni <alberto.cannoni@outlook.it>
2024-04-26 18:43:53 +0200

    update script

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //innesco init
 window.addEventListener('load', init);
 
+const portfolio = document.querySelector('#Project')
+
 function refreshPage() {
     location.reload();
 }
@@ -9,4 +11,4 @@ function init(){
     console.log('init');}
 
 
-let comingSoon = document.addEventListener('click', ()=>{alert("Coming soon!")});
+let comingSoon = portfolio.addEventListener('click', ()=>{alert("Coming soon!")});
